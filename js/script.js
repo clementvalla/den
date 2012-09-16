79a771b5d6d0e56c21a762dc832552f1ad48bf84
clementvalla <clementvalla@gmail.com>
2012-09-16 11:34:18 -0400

    clement canvas changes

--- js/script.js
+++ js/script.js
@@ -11,37 +11,52 @@ function setup() {
 
 function draw() {
 	canvas.width = canvas.width;
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+	radius = Math.min(canvas.width*0.4, canvas.height*0.4);
+
+	//console.log("radius: "+radius);
+
+	//var wid = canvas.width;
+	//var height = canvas.height;
 
 	//update the spread
 	spread = parseInt($("input#spread").val());
-	if (!spread) spread = 100;
+	//if (!spread) spread = 75;
+	spread = (spread > 100) ? 100 : spread;
+	spread = (spread <= 0) ? 0 : spread;
+	
+
+	var offset = radius*(100-spread)/100;
 
 	//circle 1
 	ctx.beginPath();
-	ctx.arc(canvas.width/2-spread/2,canvas.height/2, radius, 0, Math.PI*2, true);
+	ctx.arc(canvas.width/2-offset,canvas.height/2, radius, 0, Math.PI*2, true);
 	ctx.closePath();
 	ctx.fillStyle = 'rgba(1,0,0,0.3)';
 	ctx.fill();
 
 	//circle 2
 	ctx.beginPath();
-	ctx.arc(canvas.width/2+spread/2,canvas.height/2, radius, 0, Math.PI*2, true);
+	ctx.arc(canvas.width/2+offset,canvas.height/2, radius, 0, Math.PI*2, true);
 	ctx.closePath();
 	ctx.fillStyle = 'rgba(1,0,0,0.3)';
 	ctx.fill();
 
-	ctx.font = "bold 12px sans-serif";
+	var font_size = radius/10;
+
+	ctx.font = "bold "+ font_size+"px sans-serif";
 	ctx.textAlign = "center";
 
-	var remainder = radius - spread/2;
+	var remainder = radius - offset;
 	
 	//text for circle 1
 	var src1 = $("input#firstcircle").val();
-	ctx.fillText(src1, canvas.width/2-spread/2-remainder, canvas.height/2);
+	ctx.fillText(src1, canvas.width/2-offset-remainder, canvas.height/2);
 
 	//text for circle 2
 	var src2 = $("input#secondcircle").val();
-	ctx.fillText(src2, canvas.width/2+spread/2+remainder, canvas.height/2);
+	ctx.fillText(src2, canvas.width/2+offset+remainder, canvas.height/2);
 
 	//text for overlap
 	var src3 = $("input#overlap").val();
