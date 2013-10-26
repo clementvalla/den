d6cd798ab34237f933fde4a45e39a46412c8a96f
jsnhff <jsnhff@gmail.com>
2013-10-25 22:14:24 -0400

    Updating About page.

--- venn-about.php
+++ venn-about.php
@@ -1,6 +1,6 @@
 <?php include 'includes/head.php';?>
 <div class="about">
-       <h1>About</h1>
+       <h1>About Us</h1>
        <p>Venn diagrams or set diagrams are diagrams that show all possible 
                logical relations between a finite collection of sets (aggregation 
                of things). Venn diagrams were conceived around 1880 by John Venn. 
