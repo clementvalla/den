061f320ac8b8a1bb3a4701e06cc73cebd2779bd8
jsnhff <jsnhff@gmail.com>
2014-08-30 00:44:18 -0400

    Fixing viewport.

--- includes/head.php
+++ includes/head.php
@@ -11,10 +11,7 @@
 	<meta name="description" content="">
 	<meta name="author" content="">
 
-    <!--
-        <meta name="viewport" content="width=device-width">
-    -->
-    <meta name="viewport" content="maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<link rel="stylesheet" href="css/jquery-ui-1.8.23.custom.css">
     <link rel="stylesheet" href="css/basscss.css">
