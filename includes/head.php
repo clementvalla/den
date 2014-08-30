ac41a7913d21aab89aba2cccfb42209dedf88c81
jsnhff <jsnhff@gmail.com>
2014-08-30 01:43:28 -0400

    Updating app icons and adding favicon.

--- includes/head.php
+++ includes/head.php
@@ -9,7 +9,9 @@
 
 	<title>The Venn Diagram Generator</title>
 	<meta name="description" content="">
-	<meta name="author" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
